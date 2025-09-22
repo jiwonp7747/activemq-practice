6974f0e7058b2e8737274011021652be150b6633
jiwonp7747 <angry9908@gmail.com>
2025-09-22 13:28:57 +0900

    setting: project init

---
+++ src/test/java/org/example/queuepractice/QueuePracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.queuepractice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QueuePracticeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
