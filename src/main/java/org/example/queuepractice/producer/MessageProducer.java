85afbd219db4baf8f88dbd19d09b75a72936d904
jiwonp7747 <angry9908@gmail.com>
2025-09-22 15:59:22 +0900

    refactor: value 주입 적용

--- src/main/java/org/example/queuepractice/producer/MessageProducer.java
+++ src/main/java/org/example/queuepractice/producer/MessageProducer.java
@@ -1,10 +1,14 @@
 package org.example.queuepractice.producer;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.jms.core.JmsTemplate;
 import org.springframework.stereotype.Service;
 
 @Service
 public class MessageProducer {
+
+    @Value("${spring.activemq.destination}")
+    private String destination;
 
     private final JmsTemplate jmsTemplate;
 
@@ -13,6 +17,6 @@ public class MessageProducer {
     }
 
     public void sendMessage(String message) {
-        jmsTemplate.convertAndSend("sample.queue", message);
+        jmsTemplate.convertAndSend(destination, message);
     }
 }
